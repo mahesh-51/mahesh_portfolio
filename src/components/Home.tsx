fa5220b4dde709ce6e5432774285026d479b74c8
Mahesh <mj7267889@gmail.com>
2023-12-10 17:41:22 +0530

    design changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,23 +2,24 @@ import Image from 'next/image'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Logo from "../images/DevsMeh.png";
+import { Card, CardBody } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <Carousel>
-      <div>
-        <hr className='bg-white' />
-        <Image
-          alt="NextUI hero Image"
-          src={Logo}
-          className="text-center"
-        />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div className='bg-dark'>
-        hello
-        <p className="legend">Legend 2</p>
-      </div>
-    </Carousel>
+    // <Carousel>
+      <main className="bg-[#853af0] font-mono">
+        <div className="grid gap-4 grid-cols-2">
+          <div className='my-5 flex justify-center'>
+            <p className='mt-8 text-white text-2xl'>
+              A Full Stack Developer proficient <br /> 
+              in Next.js, React, Redux, TypeScript, Express, <br /> 
+              Node.js, MongoDB, React Native, and UI/UX design.
+            </p>
+          </div>
+          <div>
+            hello
+          </div>
+        </div>
+      </main>
   )
 }
