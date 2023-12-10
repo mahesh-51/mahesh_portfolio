7f0b988ef46180c3dd22ba0c021ef744e18ae10b
Mahesh <mj7267889@gmail.com>
2023-12-10 16:28:53 +0530

    set up for portfolio

---
+++ src/app/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function footer() {
+
+
+  return (
+   <>
+    <div className="bg-black">
+        <p className="text-center text-white">Footer</p>
+    </div>
+   </>
+  );
+}
---
+++ src/components/Layout.tsx
@@ -0,0 +1,41 @@
+// @/components/Layout/index.tsx
+"use client";
+import React, { useState, ReactNode, useEffect } from "react";
+import Head from "next/head";
+import NextTopLoader from "nextjs-toploader";
+import { NextUIProvider } from "@nextui-org/react";
+import * as NProgress from "nprogress";
+import { usePathname } from "next/navigation";
+import Header from '../app/header'
+import Footer from '../app/footer'
+interface LayoutProps {
+    readonly pageTitle?: string;
+    children: ReactNode;
+}
+
+export default function Layout({ pageTitle, children }: LayoutProps) {
+    // Concatenate page title (if exists) to site title
+    let titleConcat = "Responsive Sidebar Example";
+    if (pageTitle) titleConcat = pageTitle + " | " + titleConcat;
+
+    // Mobile sidebar visibility state
+    const [showSidebar, setShowSidebar] = useState(false);
+    const pathname = usePathname();
+    useEffect(() => {
+        NProgress.done();
+    }, [pathname]);
+
+    return (
+        <NextUIProvider>
+            <NextTopLoader color="orange" />
+            <Head>
+                <title className="text-white">{titleConcat}</title>
+            </Head>
+            <Header />
+            <div className="max-md:mt-16 flex flex-col flex-grow w-screen md:w-full min-h-screen">
+            {children}
+            </div>
+            <Footer />
+        </NextUIProvider>
+    );
+}
